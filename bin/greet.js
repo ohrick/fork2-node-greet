9ada054383c9a19eaec9eaabd061e70e4b2fbaea
cassieword <zhangxinyu019@gmail.com>
2014-05-02 13:31:03 +0800

    first commit

---
+++ bin/greet.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var greet = require('../index.js')
+
+var parsedArgv = require('minimist')(process.argv.slice(2))
+
+console.log("Hello World")
+
+console.log(process.argv)
+
+console.log(parsedArgv)
+
+console.log(greet(parsedArgv._, parsedArgv.drunk))
