f1ea5f3051a37637cf96003969fd528a3c5e274b
cassieword <zhangxinyu019@gmail.com>
2014-05-03 12:40:01 +0800

    Setting Up Mocha For Testing

---
+++ test/greet_spec.js
@@ -0,0 +1,10 @@
+var greet = require('../index.js')
+
+describe('greet', function(){
+	it("should greet a person by name", function(){
+		expect(greet('cassie')).to.eql('hello, cassie');
+	});
+	it("should greet a person flirtatiously if drunk", function(){
+		expect(greet('cassie', 'drunk')).to.eql('hello cassie, you look sexy today');
+	});
+});
